90718d5139be25f15823a1009a98e9d4437c8b4f
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2023-03-14 18:29:22 +0200

    add some clarifications

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,6 +5,7 @@ import User from "../models/User.js";
 /* REGISTER*/
 export const register = async (req, res) => {
   try {
+    // Отримання даних з тіла запиту
     const {
       firstName,
       lastName,
@@ -16,9 +17,11 @@ export const register = async (req, res) => {
       occupation,
     } = req.body;
 
+    // Хешування паролю
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
+    // Створення нового користувача
     const newUser = new User({
       firstName,
       lastName,
@@ -31,6 +34,8 @@ export const register = async (req, res) => {
       viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     });
+
+    // Збереження користувача у базі даних
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
@@ -41,14 +46,21 @@ export const register = async (req, res) => {
 /* LOGIN */
 export const login = async (req, res) => {
   try {
+    // Отримання даних з тіла запиту
     const { email, password } = req.body;
+
+    // Пошук користувача у базі даних
     const user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
+    // Перевірка введеного паролю
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
+    // Створення токену доступу
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    // Відправлення токену та даних користувача
     delete user.password;
     res.status(200).json({ token, user });
   } catch (err) {
--- server/index.js
+++ server/index.js
@@ -45,6 +45,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/login", login);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
